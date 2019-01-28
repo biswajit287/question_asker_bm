9f376d32d37a4392fde1112b55750cbd0aa4745a
Biswajit Mondal <biswajitmandal2807@gmail.con>
2019-01-28 18:53:07 +0400

    Version Changed and Promter class changed to Asker

--- example/main.dart
+++ example/main.dart
@@ -1,14 +1,16 @@
+///imporing the rqquired files
 import 'package:question_asker_bm/question_asker_bm.dart';
 
+///start of main function
 void main()
 {
 	final options = [new Option('I like Green', '#00ff00'),
 					new Option('I like White', '#ffffff')];
 
-	final promter = new Promter();
-	String choosenAns = promter.askMultiple('What color do you like?', options);
+	final asker = new Asker();
+	String choosenAns = asker.askMultiple('What color do you like?', options);
 
-	bool ansBinary = promter.askBinary('Do you like This Lib?');
+	bool ansBinary = asker.askBinary('Do you like This Lib?');
 
 	print(choosenAns);
 	print(ansBinary);
